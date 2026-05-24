cf1b4f0469905811326afad9c3cc11294b311795
ivansabolic <60968982+ivansabolic@users.noreply.github.com>
2026-05-24 19:44:03 +0000

    Deploying to gh-pages from @ ivansabolic/ivansabolic.github.io@b645f5699e6af70a4c4b94f7b0f39aa001782f1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,7 +76,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=6_sd6kgAAAAJ", "_blank");
         },
       },{
         id: 'social-x',
